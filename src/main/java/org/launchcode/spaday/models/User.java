811cbcc7a0e36187f8e11732ccfa057b48e47db8
Alex <alexmdalton@sbcglobal.net>
2022-10-10 19:37:36 -0500

    Partway through studio

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,39 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+
 public class User {
+
+    @NotBlank(message = "Username is required.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Invalid email.")
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 6, message = "Password must be at least 6 characters long.")
     private String password;
+
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
+    }
+
+    private void checkPassword(){
+        if(!(password == verifyPassword) || password == null || verifyPassword == null){
+            verifyPassword = null;
+        }
     }
 
     public String getUsername() {
@@ -38,5 +58,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() { return verifyPassword; }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
